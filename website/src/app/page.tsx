861f3852edb1d6c7cacf712ddc56a2710b25086a
cuijiaheng0001 <cuijiaheng0001@gmail.com>
2025-07-27 00:05:36 -0700

    只保留主页第一屏轮播图内容
    - 只保留HeroCarousel组件
    - 删除所有其他主页组件（ProblemRevealSection、ServiceShowcase等）
    - 现在主页只有第一屏的轮播图内容

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,36 +1,9 @@
 import HeroCarousel from '@/components/HeroCarousel'
-import ProblemRevealSection from '@/components/ProblemRevealSection'
-import ServiceShowcase from '@/components/ServiceShowcase'
-import SimpleRecoveryFlow from '@/components/SimpleRecoveryFlow'
-// import GlobalRecoveryJourney from '@/components/GlobalRecoveryJourney'
-// import VisualRecoveryFlow from '@/components/VisualRecoveryFlow'
-// import EnhancedGlobalFlow from '@/components/EnhancedGlobalFlow'
-// import AnimatedGlobe from '@/components/AnimatedGlobe'
-// import GlobalProcessFlow from '@/components/GlobalProcessFlow'
-import EducationSection from '@/components/EducationSection'
-import ServiceTabs from '@/components/ServiceTabs'
-import ProcessAnimation from '@/components/ProcessAnimation'
-import MetricsSection from '@/components/MetricsSection'
-import CaseStudies from '@/components/CaseStudies'
-import TrustIndicators from '@/components/TrustIndicators'
-import CTASection from '@/components/CTASection'
-import RiskAssessmentButton from '@/components/RiskAssessmentButton'
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroCarousel />
-      <ProblemRevealSection />
-      <ServiceShowcase />
-      <SimpleRecoveryFlow />
-      <EducationSection />
-      <ServiceTabs />
-      <ProcessAnimation />
-      <MetricsSection />
-      <CaseStudies />
-      <TrustIndicators />
-      <CTASection />
-      <RiskAssessmentButton variant="floating" />
     </main>
   )
 }
